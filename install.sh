4114529da8d2cb9502f0d16309d348af84adcd42
dlkmp <david@lauka.mp>
2024-06-23 19:27:35 +0200

    greatly reduced install.sh for the time being

--- install.sh
+++ install.sh
@@ -2,29 +2,18 @@
 
 # Automated script to install my dotfiles
 
-# Clone dotfiles
+# Determine dotfiles dir
 if [ $# -gt 0 ]
   then
     SCRIPT_DIR=$1
   else
     SCRIPT_DIR=~/.dotfiles
 fi
-nix-shell -p git --command "git clone git@github.com:dlaukamp/nix-config.git $SCRIPT_DIR"
 
-# Check if uefi or bios
-if [ -d /sys/firmware/efi/efivars ]; then
-    sed -i "0,/bootMode.*=.*\".*\";/s//bootMode = \"uefi\";/" $SCRIPT_DIR/flake.nix
-else
-    sed -i "0,/bootMode.*=.*\".*\";/s//bootMode = \"bios\";/" $SCRIPT_DIR/flake.nix
-    grubDevice=$(findmnt / | awk -F' ' '{ print $2 }' | sed 's/\[.*\]//g' | tail -n 1 | lsblk -no pkname | tail -n 1 )
-    sed -i "0,/grubDevice.*=.*\".*\";/s//grubDevice = \"\/dev\/$grubDevice\";/" $SCRIPT_DIR/flake.nix
-fi
-
-# Patch flake.nix with different username/name and remove email by default
-sed -i "0,/dlkmp/s//$(whoami)/" $SCRIPT_DIR/flake.nix
-sed -i "0,/Dlkmp/s//$(getent passwd $(whoami) | cut -d ':' -f 5 | cut -d ',' -f 1)/" $SCRIPT_DIR/flake.nix
-sed -i "s/david@lauka.mp//" $SCRIPT_DIR/flake.nix
-sed -i "s+~/.dotfiles+$SCRIPT_DIR+g" $SCRIPT_DIR/flake.nix
+# Patch flake.nix with different username/name
+# sed -i "0,/dlkmp/s//$(whoami)/" $SCRIPT_DIR/flake.nix
+# sed -i "0,/Dlkmp/s//$(getent passwd $(whoami) | cut -d ':' -f 5 | cut -d ',' -f 1)/" $SCRIPT_DIR/flake.nix
+# sed -i "s+~/.dotfiles+$SCRIPT_DIR+g" $SCRIPT_DIR/flake.nix
 
 # Open up editor to manually edit flake.nix before install
 if [ -z "$EDITOR" ]; then
@@ -33,7 +22,7 @@ fi
 $EDITOR $SCRIPT_DIR/flake.nix;
 
 # Rebuild system
-sudo nixos-rebuild switch --flake $SCRIPT_DIR#system;
+# sudo nixos-rebuild switch --flake $SCRIPT_DIR;
 
 # Install and build home-manager configuration
-nix run home-manager/master --extra-experimental-features nix-command --extra-experimental-features flakes -- switch --flake $SCRIPT_DIR#dlkmp;
+nix run home-manager/master --extra-experimental-features nix-command --extra-experimental-features flakes -- switch --flake $SCRIPT_DIR;
